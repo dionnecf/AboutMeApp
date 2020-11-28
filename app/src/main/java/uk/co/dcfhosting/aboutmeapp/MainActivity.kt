2f467a56c8e5319b49ed2bbc4e5307bae78f6a5a
Dionne Condor-Farrell <info@dcfhosting.co.uk>
2020-11-28 18:26:01 +0000

    added new activity_main layout

---
+++ app/src/main/java/uk/co/dcfhosting/aboutmeapp/MainActivity.kt
@@ -0,0 +1,10 @@
+package uk.co.dcfhosting.aboutmeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
